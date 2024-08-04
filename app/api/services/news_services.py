68f2cdec878652ee45d2b2612f528a97f3619ba6
Alexandur11 <Alexandurdaskalov11@gmail.com>
2024-08-04 10:38:17 +0300

    Parse time from news fetch

--- app/api/services/news_services.py
+++ app/api/services/news_services.py
@@ -1,11 +1,12 @@
 from app.api.fetches.news_fetches import fetch_news_from_alpha_vantage
-import pandas as pd
+from datetime import  datetime
 
 def alpha_vantage_news(symbol:str):
     data = fetch_news_from_alpha_vantage(symbol)
+    times = [(x[0:4] + " " + x[4:6] + "." + x[6:8]) for x in data['time_published']]
     news = {'Title':data['title'],
             'Url':data['url'],
-            'Time':data['time_published'],
+            'Time':times,
             'Image':data['banner_image']}
 
     return news
